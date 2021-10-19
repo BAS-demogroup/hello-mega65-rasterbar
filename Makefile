e9d7a4305e8f60ec6ae31d07f06c4d1cf9da6449
falken <532282+falken42@users.noreply.github.com>
2021-10-19 14:19:08 +0900

    optimize irq handler a bit

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: test d81 testd81 clean
+.PHONY: test d81 d81z testd81 clean
 
 # Configurable Section
 ###########################################
@@ -12,6 +12,7 @@ MAIN_SRC_FILE	:= $(DIR_SRC)/main.asm
 TARGET_BASENAME	:= $(DIR_BUILD)/$(PROJECT_NAME)
 TARGET		:= $(TARGET_BASENAME).prg
 IMAGE_D81	:= $(TARGET_BASENAME).d81
+IMAGE_D81_ZIP	:= $(TARGET_BASENAME).zip
 
 PWD		:= $(shell pwd)
 
@@ -28,6 +29,12 @@ $(IMAGE_D81): $(TARGET)
 	c1541 -format diskname,id d81 $(PWD)/$@ -attach $(PWD)/$@ -write $(PWD)/$< $(PROJECT_NAME)
 
 d81: $(IMAGE_D81)
+
+$(IMAGE_D81_ZIP): $(IMAGE_D81)
+	@rm -f $@
+	zip -9jv $@ $<
+
+d81z: $(IMAGE_D81_ZIP)
 
 testd81: $(IMAGE_D81)
 	xmega65 -fullborders -besure -autoload -8 $<
--- src/main.asm
+++ src/main.asm
@@ -53,15 +53,14 @@ irq_handler:
 	; save registers (faster than pha/txa/etc)
 	sta .irq_temp
 	stx .irq_temp+1
-	sty .irq_temp+2
 
 	; acknowledge the VIC interrupt
 	lda #$ff
 	sta $d019
 
-	; read color for this raster line we're currently processing
+	; read color from buffer for this raster line we're currently processing
 	ldx .rb_line
-	ldy .rb_line+1
+	lda .rb_line+1
 	beq irq_rb_line_low
 	lda .rb_buffer+256, x
 	jmp irq_rb_set
@@ -75,41 +74,41 @@ irq_rb_set:
 	sta $d021
 
 	; increment to next raster line
+	ldx .rb_line
 	lda .rb_line+1
 	beq irq_inc_low
 
 	; check if we hit the last screen line (PAL has 312 lines, so 312-256=56)
-	inc .rb_line
-	lda .rb_line
-	cmp #56
-	bne irq_set_vic
+	inx
+	cpx #56
+	bne irq_set_vic_line
 
 	; we hit the last line, so reset to line zero
 	lda #0
 	sta .rb_line
 	sta .rb_line+1
-	jmp irq_set_vic
+	sta $d012
+	lda #$1b
+	sta $d011
+	jmp irq_done
 
 irq_inc_low:
-	inc .rb_line
-	bne irq_set_vic
-	lda #$80
-	eor .rb_line+1
-	sta .rb_line+1
+	inx
 
-irq_set_vic:
-	; update VIC raster line irq
-	lda .rb_line
-	sta $d012
-	lda .rb_line+1
-	ora #$1b
+irq_set_vic_line:
+	stx .rb_line
+	stx $d012
+	bne irq_done
+
+	; handle rollover (screen line 255 -> 256)
+	inc .rb_line+1
+	lda #$9b				; set high bit (bit 7) in VIC for irqs on raster lines >= 256
 	sta $d011
 
 irq_done:
 	; restore registers and return
 	lda .irq_temp
 	ldx .irq_temp+1
-	ldy .irq_temp+2
 	rti
 
 .rb_line	!word $00
